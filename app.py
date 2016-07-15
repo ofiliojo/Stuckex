a1aa3395530d3da545c57d9f3164ebd59550efc2
Ofiliojo <ofiliojo@gmail.com>
2016-07-15 14:30:44 -0400

    Error Handling

--- app.py
+++ app.py
@@ -30,41 +30,59 @@ def searchForTicker():
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
         data = json.loads(the_page.decode('utf-8'))
-    session['ticker'] = {'name': data.get(session['theSymbol']).get('name'),
-                            'bidPrice': data.get(session['theSymbol']).get('bidPrice'),
-                                'askPrice': data.get(session['theSymbol']).get('askPrice'),
-                                    'quantityOwned' : 0}
-    return render_template('result.html', ticker = session['ticker'], cash = session['cashBalance'], portfolio = session['portfolioOfCompanies'])
+    if data.get("null") or data == {}:
+        session['searchError'] = "Please Enter a Valid Ticker"
+        return render_template('index.html', searchError = session['searchError'], cash = session['cashBalance'], portfolio = session['portfolioOfCompanies'])
+    else:
+        session['searchError'] = ""
+        session['ticker'] = {'name': data.get(session['theSymbol']).get('name'),
+                                'bidPrice': data.get(session['theSymbol']).get('bidPrice'),
+                                    'askPrice': data.get(session['theSymbol']).get('askPrice'),
+                                        'quantityOwned' : 0}
+        return render_template('result.html', searchError = session['searchError'], ticker = session['ticker'], cash = session['cashBalance'], portfolio = session['portfolioOfCompanies'])
 @app.route("/buy", methods=['POST'])
 def buy():
     session['numberOfStocksToBuy'] = request.form["numberOfStocksToBuy"]
-    numberToBuy = int(session['numberOfStocksToBuy'])
-    costOfStocks = int(session['ticker'].get('askPrice')) * numberToBuy
-    if costOfStocks <= session['cashBalance']:
-        if not any(stock.get('name', None) == session['ticker'].get('name') for stock in session['portfolioOfCompanies']):
-            session['ticker']['quantityOwned'] = numberToBuy
-            session['cashBalance'] = session['cashBalance'] - costOfStocks
-            session['portfolioOfCompanies'].append(session['ticker'])
-        else:
-            for stock in session['portfolioOfCompanies']:
-                if stock['name'] == session['ticker']['name']:
-                    stock['quantityOwned'] = stock['quantityOwned']+numberToBuy
+    if session['numberOfStocksToBuy'].isdigit():
+        numberToBuy = int(session['numberOfStocksToBuy'])
+        session['buyError'] = ""
+        costOfStocks = int(session['ticker'].get('askPrice')) * numberToBuy
+        if costOfStocks <= session['cashBalance']:
+            if not any(stock.get('name', None) == session['ticker'].get('name') for stock in session['portfolioOfCompanies']):
+                session['ticker']['quantityOwned'] = numberToBuy
                 session['cashBalance'] = session['cashBalance'] - costOfStocks
-    return render_template('result.html', ticker = session['ticker'], cash = session['cashBalance'], portfolio = session['portfolioOfCompanies'])
+                session['portfolioOfCompanies'].append(session['ticker'])
+            else:
+                for stock in session['portfolioOfCompanies']:
+                    if stock['name'] == session['ticker']['name']:
+                        stock['quantityOwned'] = stock['quantityOwned']+numberToBuy
+                    session['cashBalance'] = session['cashBalance'] - costOfStocks
+        else:
+            session['buyError'] = "Not Enough Buy"
+
+    else:
+        session['buyError'] = "Enter Valid Amount"
+    return render_template('result.html', buyError = session['buyError'], ticker = session['ticker'], cash = session['cashBalance'], portfolio = session['portfolioOfCompanies'])
 
 @app.route("/sell", methods=['POST'])
 def sell():
     session['numberOfStocksToSell'] = request.form["numberOfStocksToSell"]
-    numberToSell = int(session['numberOfStocksToSell'])
-    costOfStocks = int(session['ticker'].get('bidPrice')) * numberToSell
-    for stock in session['portfolioOfCompanies']:
-        if stock['name'] == session['ticker']['name']:
-            if numberToSell <= stock['quantityOwned']:
-                stock['quantityOwned'] = stock['quantityOwned']-numberToSell
-                session['cashBalance'] = session['cashBalance'] + costOfStocks
-                if stock['quantityOwned'] == 0:
-                    session['portfolioOfCompanies'][:] = [stock for stock in session['portfolioOfCompanies'] if stock.get('name') != session['ticker'].get('name')]
-    return render_template('result.html', ticker = session['ticker'], cash = session['cashBalance'], portfolio = session['portfolioOfCompanies'])
+    if session['numberOfStocksToSell'].isdigit():
+        session['sellError'] = ""
+        numberToSell = int(session['numberOfStocksToSell'])
+        costOfStocks = int(session['ticker'].get('bidPrice')) * numberToSell
+        for stock in session['portfolioOfCompanies']:
+            if stock['name'] == session['ticker']['name']:
+                if numberToSell <= stock['quantityOwned']:
+                    stock['quantityOwned'] = stock['quantityOwned']-numberToSell
+                    session['cashBalance'] = session['cashBalance'] + costOfStocks
+                    if stock['quantityOwned'] == 0:
+                        session['portfolioOfCompanies'][:] = [stock for stock in session['portfolioOfCompanies'] if stock.get('name') != session['ticker'].get('name')]
+                else:
+                    session['sellError'] = "Not Enough Stock"
+    else:
+        session['sellError'] = "Enter Valid Amount"
+    return render_template('result.html', sellError = session['sellError'], ticker = session['ticker'], cash = session['cashBalance'], portfolio = session['portfolioOfCompanies'])
 
 
 
